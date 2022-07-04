605894c0e3aef3a221a71fa3b4b60a8f5ba5848d
Morpa <andremorpa@gmail.com>
2022-07-04 20:23:15 +0100

    Initial config

---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+    </>
+  )
+]
---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/styles.ts',
+    '!dist',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+}
